61eb42e492e7e9900f5a5666848f240b7a8046a5
guganravi06 <105877037+guganravi06@users.noreply.github.com>
2024-01-29 23:15:47 +0530

    Update server.js
    port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const twilio = require('twilio');
 const cors = require('cors');  // Import cors module
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());  // Enable CORS for all routes
